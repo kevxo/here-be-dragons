fdbd49cfa7fb647c4789d42d1cc4603fc2c293c6
Kevin Cuadros <kev040798@gmail.com>
2020-11-12 14:09:14 -0500

    Finish exercises

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -12,6 +12,7 @@ class NytimesTest < Minitest::Test
 
   def test_it_can_get_copyright
     #Set your code to the local variable, "result"
+    result = @hash[:copyright]
 
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
@@ -19,13 +20,17 @@ class NytimesTest < Minitest::Test
   def test_it_can_get_array_of_stories
     #Set your code to the local variable, "result"
 
+    result = @hash[:results]
+
     assert result.is_a? (Array)
     assert_equal 44, result.count
   end
 
   def test_it_can_get_all_stories_with_subsection_of_politics
     #Set your code to the local variable, "result"
-
+    result = @hash[:results].find_all do |results|
+      results[:section] == "U.S." && results[:subsection] == 'Politics'
+    end
 
     assert result.is_a? (Array)
     assert_equal 6, result.count
